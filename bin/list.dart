f46a7415cd9296331669b989673f2e957654ec0f
rikkyps <rikky.putra14@gmail.com>
2021-10-21 13:43:14 +0700

    list

---
+++ bin/list.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<int> myList = []; //List Kosong
+  List<int> list = [1, 2, 3];
+
+  list.add(6);
+  myList.addAll(list);
+  if (myList.contains(6)) {
+    print('Betul');
+  }
+
+  myList.forEach((element) {
+    print(element);
+  });
+}
